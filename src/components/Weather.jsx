97d2d96f90d697b138d533be484afe96c4d15aec
gize22 <gizachewkassa22@gmail.com>
2026-04-23 17:44:56 +0000

    ui working

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,7 +10,19 @@ const Weather = () => {
          <input type='text' placeholder='search'/> 
          <img className='img' src={searchIcon} alt='' />
       </div>
-
+      <img className='weatherIcon' src='https://cdn-icons-png.flaticon.com/512/1163/1163661.png' alt='' />
+      <p className='temperature'>20°c</p>
+      <p className='location'>addis ababa</p>
+    <div className="weather-data">
+        <div className="col">
+          <p>91%</p>
+          <span>Humidity</span>
+         </div>
+        <div className="col">
+           <p>10 km/h</p>
+           <span>Wind Speed</span>
+        </div>
+      </div>
     </div>
   );
 };    
