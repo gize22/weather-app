950361bb964b703f440f9b7341d16591988b68e0
gize22 <gizachewkassa22@gmail.com>
2026-04-23 18:13:34 +0000

    weather api working on the console

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Weather.css';
 import searchIcon from '../assets/search-icon.jpg';
 
 
 const Weather = () => {
+
+   const search = async (city) => {
+    try {
+      const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f81291a41ee7debdf8dd0390425e6cc&units=metric`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+
+    }
+  
+          useEffect(() => {
+            search("london");
+              }, [])
+
   return (
     <div className='weather'>
       <div className="searchBar">
@@ -11,7 +28,7 @@ const Weather = () => {
          <img className='img' src={searchIcon} alt='' />
       </div>
       <img className='weatherIcon' src='https://cdn-icons-png.flaticon.com/512/1163/1163661.png' alt='' />
-      <p className='temperature'>20°c</p>
+      <p className='temperature'>20°C</p>
       <p className='location'>addis ababa</p>
     <div className="weather-data">
         <div className="col">
